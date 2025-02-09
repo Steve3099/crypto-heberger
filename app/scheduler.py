015fd105e85a2d61e55470323eebc357a9a3d756
Steve <ramiandrisoasteve@gmail.com>
2025-02-09 20:03:00 +0300

    index

--- app/scheduler.py
+++ app/scheduler.py
@@ -14,7 +14,7 @@ def my_cron_task():
     print(f"Running cron task at {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 # Schedule the cron job
-scheduler.add_job(indexService.set_Index, "interval", minutes=5)
+scheduler.add_job(indexService.set_Index, "interval", minutes=120)
 scheduler.add_job(coinGeckoService.schedule_historique_prix, "cron", hour=0, minute=10)
 scheduler.add_job(coinGeckoService.schedule_market_cap, "cron", hour=0, minute=1)
 
